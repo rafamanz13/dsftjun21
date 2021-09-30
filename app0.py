e15416e57e63a118fbbe8b5a364b2500fe61be34
rafamanz13 <rafamanz.13@gmail.com>
2021-09-30 10:45:05 +0000

    cambio lololololo

--- app0.py
+++ app0.py
@@ -6,6 +6,6 @@ app.config["DEBUG"] = True
  
 @app.route('/', methods=['GET'])
 def home():
-	return "<h1>Distant Reading Archive lalalalala </h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+	return "<h1>Distant Reading Archive lololololololo </h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
  
 app.run()
